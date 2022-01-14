7ca1d34f5ebd5871f5722a9df4a46690db9b3c2d
OlgaVdovychenko <97684791+OlgaVdovychenko@users.noreply.github.com>
2022-01-14 18:23:59 +0200

    Update SmartCalculator.py

--- SmartCalculator.py
+++ SmartCalculator.py
@@ -3,60 +3,61 @@ import re
 
 
 def print_help():
+    """
+    Function describe the work of SmartCalculator
+    """
     print("The program calculates the sum or subtraction of numbers")
     print("It supports both unary and binary minus operators")
-    print("Remember: two adjacent minus signs turn into a plus.")
+    print("Remember: two adjacent minus signs turn into a plus")
+    print('The program calculates the arithmetical expression')
+    print('It supports all arithmetical operations and parentheses')
+    print('The calculator supports both unary and binary minus operators')
+    print('Remember: two adjacent minus signs turn into a plus.')
+    print('Sequence of * and / will result an error message')
+    print('The program supports the calculations with variables')
+    print('If you want to stop calculations, enter "/exit"')
 
 
-def do_addition(a, b):
-    return a + b
-
-
-def do_subtraction(a, b):
-    return a - b
-
-
-def do_multiplication(a, b):
-    return a * b
-
-
-def do_division(a, b):
-    try:
-        return int(a / b)
-    except ZeroDivisionError:
-        print("You can't divide by zero")
-        return None
-
-
-def do_power(a, b):
-    return a ** b
-
-
-def do_operation(operation, operand_1, operand_2):
+def do_operation(operation: str, operand_1, operand_2):
+    """
+    Function takes operator and two operands
+    and returns the result of corresponding arithmetic operation.
+    Possible operations:
+    addition: +
+    subtraction: -
+    integer division: /
+    multiplication: *
+    power: ^
+    For the case of an integer division, when division by zero,
+    the function returns None
+    :param operation: str (+, -, /, *, ^)
+    :param operand_1: int or float
+    :param operand_2: int or float
+    :return: result of operation or None
+    """
     if operation == '+':
-        return do_addition(operand_1, operand_2)
+        return operand_1 + operand_2
     elif operation == '-':
-        return do_subtraction(operand_1, operand_2)
+        return operand_1 - operand_2
     elif operation == '*':
-        return do_multiplication(operand_1, operand_2)
+        return operand_1 * operand_2
     elif operation == '/':
-        return do_division(operand_1, operand_2)
+        try:
+            return int(operand_1/operand_2)
+        except ZeroDivisionError:
+            print("You can't divide by zero")
+            return None
     elif operation == '^':
-        return do_power(operand_1, operand_2)
-
-
-def manage_calculation(num_op):
-    operand_1 = num_op.pop(0)
-    result = None
-    while len(num_op) >= 2:
-        operation = num_op.pop(0)
-        operand_2 = num_op.pop(0)
-        result = do_operation(operation, operand_1, operand_2)
-        operand_1 = result
-    return result
+        return operand_1 ** operand_2
 
 
 def is_brackets_ok(expr):
+    """
+    Function checks the correctness of parentheses placement
+    in arithmetic expression
+    :param expr:
+    :return: True (if parentheses placement is correct) or None
+    """
     brackets_stack = []
     for elem in expr:
         if elem == '(':
@@ -72,6 +73,16 @@ def is_brackets_ok(expr):
 
 
 def user_input_parsing(line):
+    """
+    Function parses the expression entered by user and returns list
+    of operands and operations in infix notation.
+    If variables are used in expression, the values of these variables
+    will be substituted.
+    In the case of incorrect expression or using the undefined variables,
+    function returns None.
+    :param line: srt
+    :return: list of operands and operations in infix notation or None
+    """
     # num_op = line.split()
     if not is_brackets_ok(line):
         print('Invalid expression')
@@ -104,25 +115,27 @@ def user_input_parsing(line):
             else:
                 print('Invalid identifier')
                 return None
-        elif not num_op[i].isdigit() and num_op[i][1:].isdigit() and not num_op[i] in '()*/^+-':
+        elif not num_op[i].isdigit() and not num_op[i][1:].isdigit() and not num_op[i] in '()*/^+-':
             print('Invalid expression')
             return None
     return num_op
 
 
 def infix_to_postfix(infix_list):
-    prec = dict()
-    prec['('] = 1
-    prec[')'] = 1
-    prec['^'] = 4
-    prec['*'] = 3
-    prec['/'] = 3
-    prec['+'] = 2
-    prec['-'] = 2
-
+    """
+    Function converts the expression in infix notation to the
+    postfix notation
+    Function takes a list of operands and operators and returns the list
+    of operands and operators in postfix notation
+    :param infix_list: list
+    :return: list of operand and operations in postfix notation
+    """
+    prec = {'(': 1, ')': 1,
+            '+': 2, '-': 2,
+            '*': 3, '/': 3,
+            '^': 4}
     postfix_list = []
     operators_stack = []
-
     for token in infix_list:
         if token.isdigit() or token[1:].isdigit():
             postfix_list.append(token)
@@ -137,14 +150,19 @@ def infix_to_postfix(infix_list):
             while operators_stack and prec[operators_stack[-1]] >= prec[token]:
                 postfix_list.append(operators_stack.pop())
             operators_stack.append(token)
-
     while operators_stack:
         postfix_list.append(operators_stack.pop())
-
     return postfix_list
 
 
 def postfix_eval(postfix_list):
+    """
+    Function evaluates the expression in postfix notation
+    Function takes the list of operands and operators in postfix
+    expression and returns the result of calculation,
+    :param postfix_list: list of operands and operators in postfix notation
+    :return: result of calculation
+    """
     operand_stack = []
     for token in postfix_list:
         if token.isdigit() or token[1:].isdigit():
@@ -158,6 +176,12 @@ def postfix_eval(postfix_list):
 
 
 def is_var_correct(var):
+    """
+    Function checks whether the name of variable is correct.
+    In variable name only letters of Latin alphabet can be used.
+    :param var: str, name of variable
+    :return: True or False
+    """
     for letter in var:
         if letter not in string.ascii_letters:
             return False
@@ -165,8 +189,11 @@ def is_var_correct(var):
 
 
 def assignment_parsing(inp_string):
+    """
+    Function parses the assignment statement and remembers
+    the values of variables
+    """
     lst = inp_string.replace(' ', '').split('=')
-    # print(lst)
     if is_var_correct(lst[0]):
         if lst[1].isdigit():
             dict_user_input[lst[0]] = lst[1]
@@ -181,68 +208,52 @@ def assignment_parsing(inp_string):
         print('Invalid identifier')
 
 
-def input_with_var(inp_string):
-    # lst = inp_string.split()
-    lst = re.findall('[*/^+-]+|[0-9]+|[A-Za-z]+|[()]+', inp_string)
-    #print(lst)
-    for i in range(len(lst)):
-        if lst[i].isalpha():
-            if is_var_correct(lst[i]):
-                if lst[i] in dict_user_input:
-                    lst[i] = dict_user_input[lst[i]]
-                else:
-                    print('Unknown variable')
-                    return None
-            else:
-                print('Invalid identifier')
-                return None
-    return lst
+def manage_calculation(line):
+    """
+    Function takes the expression and manage its processing
+    :param line: str, expression
+    :return: result of calculation
+    """
+    numbers_operations = user_input_parsing(line)
+    if numbers_operations:
+        if len(numbers_operations) == 1:
+            return numbers_operations[0]
+        else:
+            # print(manage_calculation(numbers_operations))
+            p_lst = infix_to_postfix(numbers_operations)
+            # print(p_lst)
+            return postfix_eval(p_lst)
+    return None
 
 
 dict_user_input = {}
-while True:
-    user_input = input().strip()
-    if user_input.startswith('/'):
-        if user_input == '/exit':
-            print('Bye!')
-            break
-        elif user_input == '/help':
-            print_help()
-        else:
-            print('Unknown command')
-    else:
-        if len(user_input) == 0:
-            continue
-        else:
-            if user_input[0].isalpha():
-                if user_input.isalpha():
-                    if user_input in dict_user_input:
-                        print(dict_user_input[user_input])
-                    else:
-                        print('Unknown variable')
-                elif '=' in user_input:
-                    if user_input.count('=') > 1:
-                        print('Invalid assignment')
-                    else:
-                        assignment_parsing(user_input)
-                        # print(dict_user_input)
-                else:
-                    numbers_operations = user_input_parsing(user_input)
-                    if numbers_operations:
-                        if len(numbers_operations) == 1:
-                            print(numbers_operations[0])
-                        else:
-                            # print(manage_calculation(numbers_operations))
-                            p_lst = infix_to_postfix(numbers_operations)
-                            # print(p_lst)
-                            print(postfix_eval(p_lst))
+if __name__ == "__main__":
+    while True:
+        user_input = input().strip()
+        if user_input.startswith('/'):
+            if user_input == '/exit':
+                print('Bye!')
+                break
+            elif user_input == '/help':
+                print_help()
             else:
-                numbers_operations = user_input_parsing(user_input)
-                if numbers_operations:
-                    if len(numbers_operations) == 1:
-                        print(numbers_operations[0])
+                print('Unknown command')
+        else:
+            if len(user_input) == 0:
+                continue
+            else:
+                if user_input[0].isalpha():
+                    if user_input.isalpha():
+                        if user_input in dict_user_input:
+                            print(dict_user_input[user_input])
+                        else:
+                            print('Unknown variable')
+                    elif '=' in user_input:
+                        if user_input.count('=') > 1:
+                            print('Invalid assignment')
+                        else:
+                            assignment_parsing(user_input)
                     else:
-                        # print(manage_calculation(numbers_operations))
-                        p_lst = infix_to_postfix(numbers_operations)
-                        # print(p_lst)
-                        print(postfix_eval(p_lst))
+                        print(manage_calculation(user_input))
+                else:
+                    print(manage_calculation(user_input))
